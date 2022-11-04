453d6a67646ef0480af90b7e712798e2e108b588
Irving Juárez <irvingjuarez274@gmail.com>
2022-11-03 21:52:37 -0600

    Cells converted into input boxes

--- src/components/Cell/index.tsx
+++ src/components/Cell/index.tsx
@@ -1,8 +1,23 @@
+import { useState } from "react"
 import { CellProps } from "./types"
 
 export const Cell: React.FC<CellProps> = ({ children, className }) => {
+	const [editMode, setEditMode] = useState(false)
+	const toggleEditMode = () => setEditMode(prev => !prev)
+
+	if (editMode) return (
+		<td>
+			<input
+				autoFocus
+				type="text"
+				className="w-[90px] p-1 outline-none"
+				onBlur={toggleEditMode}
+			/>
+		</td>
+	)
+
 	return (
-		<td className={className}>
+		<td className={className} onClick={toggleEditMode}>
 			<>
 				{children}
 			</>
