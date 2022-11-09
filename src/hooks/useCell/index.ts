19a4405c58710d10d2cc1cb3a939ced809889963
Irving Juárez <irvingjuarez274@gmail.com>
2022-11-09 12:43:00 -0600

    Cells are able to link each other, but just once

--- src/hooks/useCell/index.ts
+++ src/hooks/useCell/index.ts
@@ -1,11 +1,12 @@
-import { useContext, useReducer, useState } from "react"
+import React, { useContext, useReducer, useState } from "react"
 import { GridContext } from "../../contexts/GridContext"
 import { GridContent } from "../useSpreadsheet/types"
 import { gridReducer } from "@app/reducers/grid/grid.reducer"
+import { Action } from "../../types"
 
 export const useCell = (id?: string) => {
 	const gridContent = useContext(GridContext) as GridContent
-	const [gridState, dispatch] = useReducer(gridReducer, gridContent)
+	const [gridState, dispatch] = useReducer<React.Reducer<GridContent, Action>>(gridReducer, gridContent)
 
 	const [editMode, setEditMode] = useState(false)
 	const toggleEditMode = () => setEditMode(prev => !prev)
--- src/reducers/grid/grid.reducer.ts
+++ src/reducers/grid/grid.reducer.ts
@@ -2,6 +2,8 @@ import { GridContent } from "../../hooks/useSpreadsheet/types"
 import { Action } from "../../types"
 import { GridActions } from "./actions"
 
+const referenceRegexp = new RegExp("[A-Z][1-9]", "ig")
+
 export const gridReducer = (state: GridContent, action: Action) => {
 	const { type, payload } = action
 
@@ -13,6 +15,14 @@ export const gridReducer = (state: GridContent, action: Action) => {
 
 			const isOperation = value.charAt(0) === "="
 			const newState = {...state}
+			const references: string[] | null = value.match(referenceRegexp)
+
+			if (references && isOperation) {
+				references.forEach(reference => {
+					const referenceValue = state[reference.toUpperCase()].computedValue
+					value = value.replace(reference, referenceValue)
+				})
+			}
 
 			try {
 				computedValue = isOperation ? eval(value.replace("=", "")) : value
