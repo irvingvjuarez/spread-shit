c8c85a9dde88de2d1efea3726cbdae4a9d061f4f
Irving Juárez <irvingjuarez274@gmail.com>
2022-11-18 10:09:45 -0600

    Able to access up, down and right cells within the grid

--- src/services/getNewCell.ts
+++ src/services/getNewCell.ts
@@ -1,30 +1,36 @@
 type Position = "left" | "right" | "up" | "down"
 
+const getRowValue = (current: string | number, moveTo: Omit<Position, "left" | "right">) => {
+	current = Number(current)
+	const updated = moveTo === "up" ? current - 1 : current + 1
+	return String(updated)
+}
+
 export const getNewCell = (currentID: string, position: Position) => {
-	const isXAxis = (position === "left" || position === "right")
 	let newValue = "", charCode
-	const rowValue = isXAxis
-		? currentID?.substring(0, 1) as string
-		: currentID?.substring(1, 2) as string
+
+	const isXAxis = (position === "left" || position === "right")
+	const replaceFromTo: [number, number] = isXAxis ? [0,1] : [1,currentID.length]
+	const replaceable = currentID?.substring(...replaceFromTo) as string
 
 	switch(position) {
 		case "down":
-			newValue = String(Number(rowValue) + 1)
+			newValue = getRowValue(replaceable, position)
+		break;
+		case "up":
+			newValue = getRowValue(replaceable, position)
 		break;
 		case "left":
-			charCode = rowValue.charCodeAt(0)
+			charCode = replaceable.charCodeAt(0)
 			newValue = String.fromCharCode(charCode - 1)
 		break;
 		case "right":
-			charCode = rowValue.charCodeAt(0)
+			charCode = replaceable.charCodeAt(0)
 			newValue = String.fromCharCode(charCode + 1)
-		break;
-		case "up":
-			newValue = String(Number(rowValue) - 1)
 		break;
 	}
 
-	const newClassname = currentID?.replace(rowValue, newValue) as string
+	const newClassname = currentID?.replace(replaceable, newValue) as string
 	return newClassname
 	// const upCell = document.querySelector("." + upClassName)
 }
