f628155c2cd8137af075e1c2a821d22f216e542f
Irving Juárez <irvingjuarez274@gmail.com>
2022-11-22 19:08:16 -0600

    classes added when references created, but classes don't dismiss on blur

--- src/components/InputCell/index.tsx
+++ src/components/InputCell/index.tsx
@@ -1,12 +1,18 @@
+import { REFERENCE_REGEXP } from "../../globals"
 import { InputCellProps } from "./types"
 
 export const InputCell: React.FC<InputCellProps> = ({ cellID, handleBlur, inputValue, viewKeyCode }) => {
 	const watchReferences = (evt: React.ChangeEvent<HTMLInputElement>) => {
 		const { value: inputValue } = evt.target
 		if (inputValue.charAt(0) === "=") {
-			console.log({
-				inputValue
-			})
+			const referenceMatches = inputValue.match(REFERENCE_REGEXP) || []
+
+			if (referenceMatches.length > 0) {
+				referenceMatches.forEach(match => {
+					const element = document.querySelector(`.${match.toUpperCase()}`)
+					element?.classList.add("cell-highlighted")
+				})
+			}
 		}
 	}
 
--- src/globals.ts
+++ src/globals.ts
@@ -2,3 +2,5 @@ import { GridMovements } from "./types";
 
 export const GRID_MOVEMENTS: Array<GridMovements | string> = ["ArrowDown", "ArrowUp", "Tab"]
 export const SPECIAL_GRID_MOVEMENTS = ["ArrowLeft", "ArrowRight"]
+
+export const REFERENCE_REGEXP = new RegExp("[A-z]{1,1}[0-9]{1,3}", "g")
